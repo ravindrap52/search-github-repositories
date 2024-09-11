e20f6857bca30aab9cda87935c1c957c61b04b06
ravindrap52 <ravindrapallikonda@gmail.com>
2024-09-11 11:19:06 +0200

    build(env): created env files for different environments
    as this is a demo project, i'm checking in the env files, other wise they are gitignored, and a common env local file need to create and do the necessary configuration

--- quasar.config.js
+++ quasar.config.js
@@ -55,7 +55,7 @@ module.exports = configure(function (/* ctx */) {
 
       // publicPath: '/',
       // analyze: true,
-      // env: {},
+      env: require('dotenv').config({ path: ['.env', '.env.local', '.env.production'], override: true }).parsed,
       // rawDefine: {}
       // ignorePublicFolder: true,
       minify: 'terser',
