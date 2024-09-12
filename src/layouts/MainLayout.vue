95a4ad39c6d6272ddd90ee1f0a1a28599da73553
ravindrap52 <ravindrapallikonda@gmail.com>
2024-09-12 13:42:15 +0200

    fix(menu): handling menu toggling

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -3,7 +3,14 @@
     <!-- header will display a hamburger menu, and a title -->
     <Header>
       <q-toolbar>
-        <BaseButton aria-label="Menu" icon="menu" type="button" round flat></BaseButton>
+        <BaseButton
+          aria-label="Menu"
+          icon="menu"
+          type="button"
+          round
+          flat
+          @click.stop="toggleLeftDrawer"
+        ></BaseButton>
         <q-toolbar-title>
           <Heading text-to-display="DataGuard" css-classes="text-2xl text-slate-900" />
         </q-toolbar-title>
@@ -11,7 +18,7 @@
     </Header>
     <!-- imagine side bar is like a container, sidebar will have a drawer, and drawer will have a form
     and form contains filter item -->
-    <SideBar>
+    <SideBar v-if="leftDrawerOpen">
       <BaseDrawer>
         <BaseForm />
       </BaseDrawer>
@@ -24,11 +31,19 @@
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
+
 import BaseButton from '@/components/common/BaseButton.vue';
 import BaseDrawer from '@/components/common/BaseDrawer.vue';
 import BaseForm from '@/components/common/BaseForm.vue';
 import Header from '@/components/common/Header.vue';
 import Heading from '@/components/common/Heading.vue';
 import SideBar from '@/components/common/SideBar.vue';
+
+const leftDrawerOpen = ref<boolean>(true);
+
+function toggleLeftDrawer() {
+  leftDrawerOpen.value = !leftDrawerOpen.value;
+}
 
 </script>
